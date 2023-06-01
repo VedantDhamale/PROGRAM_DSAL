077a082cd1e199d02f7db04843300a45dd608b4e
Vedant <66168936+vedantdhamle9@users.noreply.github.com>
2023-06-01 22:28:15 +0530

    Update E1.cpp
    correction  related output

--- E1.cpp
+++ E1.cpp
@@ -1,10 +1,10 @@
-
 // Read the marks obtained by students of second year in an online examination of particular
 // subject. Find out maximum and minimum marks obtained in that subject. Use heap data
 // structure. Analyze the algorithm
 
 #include <iostream>
 using namespace std;
+
 class Heap
 {
     int n;
@@ -12,33 +12,35 @@ class Heap
 
 public:
     void get();
-    void displayMin() { cout << "Minimum marks are :" << minheap[0] << endl; }
-    void displayMax() { cout << "Maximum marks are :" << maxheap[0] << endl; }
+    void displayMin() { cout << "Minimum marks are: " << minheap[0] << endl; }
+    void displayMax() { cout << "Maximum marks are: " << maxheap[0] << endl; }
     void upAdjust(bool, int);
 };
+
 void Heap::get()
 {
-    cout << "Enter number of students." << endl;
+    cout << "Enter the number of students." << endl;
     cin >> n;
     int k;
     minheap = new int[n];
     maxheap = new int[n];
-    cout << "Enter marks of students." << endl;
+    cout << "Enter the marks of students." << endl;
     for (int i = 0; i < n; i++)
     {
         cin >> k;
         minheap[i] = k;
-        upAdjust(0, i);
+        upAdjust(1, i); // Adjust min heap
         maxheap[i] = k;
-        upAdjust(1, i);
+        upAdjust(0, i); // Adjust max heap
     }
 }
+
 void Heap::upAdjust(bool m, int l)
 {
     int s;
-    if (!m)
+    if (m)
     {
-        while (minheap[(l - 1) / 2] < minheap[l])
+        while (minheap[(l - 1) / 2] > minheap[l])
         {
             s = minheap[l];
             minheap[l] = minheap[(l - 1) / 2];
@@ -50,7 +52,7 @@ void Heap::upAdjust(bool m, int l)
     }
     else
     {
-        while (maxheap[(l - 1) / 2] > maxheap[l])
+        while (maxheap[(l - 1) / 2] < maxheap[l])
         {
             s = maxheap[l];
             maxheap[l] = maxheap[(l - 1) / 2];
@@ -61,11 +63,12 @@ void Heap::upAdjust(bool m, int l)
         }
     }
 }
-main()
+
+int main()
 {
     Heap H;
     H.get();
     H.displayMin();
     H.displayMax();
-    return (0);
+    return 0;
 }
