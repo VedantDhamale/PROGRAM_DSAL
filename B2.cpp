83269f67df59e6dffdd567a4869c579d27dd8ee4
Vedant <66168936+vedantdhamle9@users.noreply.github.com>
2023-06-02 23:55:33 +0530

    Update B2.cpp

--- B2.cpp
+++ B2.cpp
@@ -58,12 +58,10 @@ void mirror(Node *root)
         return;
     else
     {
-        Node *temp;
+
         mirror(root->left);
         mirror(root->right);
-        temp = root->left;
-        root->left = root->right;
-        root->right = temp;
+        swap(root->left, root->right);
     }
 }
 
